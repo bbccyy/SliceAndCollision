67f3185e0f6a5d242aa477b411af2cc5827147cd
bbccyy <yw1514@nyu.edu>
2023-03-16 15:56:09 +0800

    update 6

--- SliceAndCollision/Assets/Script/Tool/OutlineDetector.cs
+++ SliceAndCollision/Assets/Script/Tool/OutlineDetector.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 using static Babeltime.Utils.OutlineDetector;
@@ -10,6 +11,8 @@ namespace Babeltime.Utils
     public class OutlineDetector
     {
         public static ObjectPool<Cell> CellPool = new ObjectPool<Cell>(100000);
+
+        public static int seqBentSeqThreshold = 5; //判断是否构成“直线”->“外折”->“直线”的直线长度(像素个数) 
 
         private static float onePixelSize = 0;
         public static float OnePixelSize { 
@@ -30,6 +33,14 @@ namespace Babeltime.Utils
             Undefine,
             In,
             Out,
+        }
+
+        public enum OutlineState
+        {
+            Undefined = 0,
+            Sequence = 1,       //连续点 -> 对应田字内有2个(非对角)InCell -> 需记录连续个数 
+            InnerBent = 2,      //内折点 -> 对应田字内只有1个InCell
+            OutterBent = 3,     //外转点 -> 对应田字内有3个InCell
         }
 
         public enum FourDir //田字区域的各个方向 
@@ -107,6 +118,18 @@ namespace Babeltime.Utils
 
         public class Context
         {
+            public static ObjectPool<OutlineTracker> OutlineTrakerPool = new ObjectPool<OutlineTracker>(10000);
+            public class OutlineTracker : IPoolable 
+            {
+                public OutlineState st = OutlineState.Undefined;
+                public int seqCount = 0;
+                public Cell keyCell = null;
+                public void Reinit(OutlineState aState) { st = aState; seqCount = 0; }
+                public void Recycle() { keyCell = null; }
+            }
+
+            public Queue<OutlineTracker> queue = new Queue<OutlineTracker>();
+
             public Texture2D texture;
             public int texWidth = 0;
             public int texHeight = 0;
@@ -114,8 +137,8 @@ namespace Babeltime.Utils
             public CrossDir lastCrossDir; //上一个确认过的轮廓点处于当前田字测试的哪个方位(记得及时刷新) 
             public Cell nextCross;
 
-            public List<Cell> History = new List<Cell>();
-            public List<Cell> SamplePoint = new List<Cell>();
+            public List<Cell> History = new List<Cell>();  //可能存在连续2个Cell是同一个的情况(上一次内折，下一次外折) 
+            public List<Cell> SamplePoint = new List<Cell>(); 
 
             public List<Cell> WorkingFour = new List<Cell>((int)FourDir._Ct);
 
@@ -126,6 +149,7 @@ namespace Babeltime.Utils
                 texHeight = tex.height;
                 mTable = new Cell[texWidth, texHeight];
                 History.Clear();
+                queue.Clear();
                 nextCross = null;
 
             }
@@ -144,6 +168,39 @@ namespace Babeltime.Utils
                     var cell = GetOrInitCellAt(aCellX + offset.Item1, aCellY + offset.Item2);
                     WorkingFour[(int)dir] = cell;  //cell may be null 
                 }
+            }
+            public void UpdateNextWorkingFourParams(CrossDir aNextDir, Cell aCenterCell = null)
+            {
+                //是否使用输入Cell作为指定的轮廓中心点  
+                if (aCenterCell != null) nextCross = aCenterCell;
+
+                //重置参数 
+                var curCell = nextCross; //这是田字中心轮廓点对应的Cell 
+                nextCross = null;
+                lastCrossDir = CrossDir.Undifined;
+                switch (aNextDir)
+                {
+                    case CrossDir.Left:
+                        nextCross = GetOrInitCellAt(curCell.x - 1, curCell.y);
+                        lastCrossDir = CrossDir.Right;
+                        break;
+                    case CrossDir.Right:
+                        nextCross = GetOrInitCellAt(curCell.x + 1, curCell.y);
+                        lastCrossDir = CrossDir.Left;
+                        break;
+                    case CrossDir.Up:
+                        nextCross = GetOrInitCellAt(curCell.x, curCell.y + 1);
+                        lastCrossDir = CrossDir.Down;
+                        break;
+                    case CrossDir.Down:
+                        nextCross = GetOrInitCellAt(curCell.x, curCell.y - 1);
+                        lastCrossDir = CrossDir.Up;
+                        break;
+                    default:
+                        Debug.LogError("UpdateNextWorkingFourParams err");
+                        return;
+                }
+                return;
             }
             public Cell GetOrInitCellAt(int aX, int aY)
             {
@@ -167,11 +224,42 @@ namespace Babeltime.Utils
             {
                 SamplePoint.Add(aCell);
             }
-            public Cell GetFirstCell()
+            public void UpdateTracker(OutlineState aCurSt, Cell aKeyCell)
             {
-                if (History.Count == 0)
-                    return null;
-                return History[0];
+                if (queue.Count == 0)
+                {   //初始状态 
+                    OutlineTracker firstTk = OutlineTrakerPool.Get();
+                    firstTk.Reinit(aCurSt);
+                    firstTk.keyCell = aKeyCell;
+                    queue.Enqueue(firstTk);
+                    return;
+                }
+
+                var latestTk = queue.ElementAt(queue.Count - 1);
+                if (latestTk.st == OutlineState.Sequence && aCurSt == OutlineState.Sequence)
+                {   //合并连续序列
+                    latestTk.keyCell = aKeyCell;
+                    latestTk.seqCount++;
+                    return;
+                }
+
+                //向队列添加新的轮廓线追踪状态 
+                OutlineTracker nextTk = OutlineTrakerPool.Get();
+                nextTk.Reinit(aCurSt);
+                nextTk.keyCell = aKeyCell;
+                queue.Enqueue(nextTk);
+
+                if(queue.Count == 4)
+                {   //超出上限需要Dequeue，同时执行必要检测 
+                    var oldestTk = queue.Dequeue();
+                    if (oldestTk.st == OutlineState.Sequence && latestTk.st == OutlineState.Sequence
+                        && queue.Peek().st == OutlineState.OutterBent && 
+                        oldestTk.seqCount >= seqBentSeqThreshold && latestTk.seqCount >= seqBentSeqThreshold)
+                    {
+                        if (oldestTk.keyCell != null) RegisterSamplePoint(oldestTk.keyCell); //需要采样这个点 
+                    }
+                    OutlineTrakerPool.Restore(oldestTk);
+                }
             }
             public void Dispose()
             {
@@ -180,6 +268,7 @@ namespace Babeltime.Utils
                     y = History[0].y + 1; //需要多1层，鉴于Out类型的Cell会出现在FirstCell的上面 
                 History.Clear();
                 SamplePoint.Clear();
+                queue.Clear();
                 for (; y >= 0; y--)
                 {
                     bool find = false;
@@ -302,6 +391,9 @@ namespace Babeltime.Utils
             //确定下一个轮廓点中心点 
             aCtx.nextCross = firstCell; //意味着当前Cell右上角的轮廓点是下一次迭代的田字中心点 
 
+            //更新轮廓点追踪状态 
+            aCtx.UpdateTracker(OutlineState.InnerBent, firstCell); 
+
             return FSM.CalFour; //下一个状态是填充田字区域，并基于填充结果执行状态切换 
         }
 
@@ -361,13 +453,15 @@ namespace Babeltime.Utils
 
             //确定是哪一个方位的Cell 
             FourDir InCellDir = FourDir._Ct;
+            Cell InCell = null;
             foreach (FourDir dir in dirArray)
             {
                 if (dir == FourDir._Ct) continue;
-                if (aCtx.WorkingFour[(int)dir] == null) continue;
-                if (aCtx.WorkingFour[(int)dir].type == CellType.In)
+                InCell = aCtx.WorkingFour[(int)dir];
+                if (InCell == null) continue;
+                if (InCell.type == CellType.In)
                 {
-                    InCellDir = dir;
+                    InCellDir = dir; 
                     break;
                 }
             }
@@ -379,21 +473,78 @@ namespace Babeltime.Utils
             var nextDir = sub_lut[aCtx.lastCrossDir];
             if (nextDir == CrossDir.Undifined) { Debug.Log("OneInFour err"); return FSM.Error;}
 
-            //更新context
-            switch(nextDir)
+            //需要重置一下上一轮配置田字格区域的参数 
+            aCtx.UpdateNextWorkingFourParams(nextDir);
+
+            //更新轮廓线追踪状态(调用内部可能会触发输出采样)
+            aCtx.UpdateTracker(OutlineState.InnerBent, InCell); //涉及输出采样点，一律使用InCell 
+
+            //内折点需要输出采样(注意此操作需要在更新轮廓线追踪状态之后进行)
+            aCtx.RegisterSamplePoint(InCell);                   //使用InCell输出采样 
+
+            //注册访问过的Cell
+            aCtx.RegisterHistoryCell(InCell);
+
+            return FSM.CalFour;
+        }
+
+        public FSM TwoInFour(Context aCtx)
+        {
+            if (aCtx.lastCrossDir == CrossDir.Undifined) { Debug.Log("TwoInFour err"); return FSM.Error; }
+
+            //确定是哪一个方位的Cell 
+            FourDir InCellDir = FourDir._Ct;
+            Cell InCell = null; 
+            Cell lastHistoryCell = aCtx.History[aCtx.History.Count - 1]; 
+            foreach (FourDir dir in dirArray)
             {
-                case CrossDir.Left:
+                if (dir == FourDir._Ct) continue;
+                InCell = aCtx.WorkingFour[(int)dir];
+                if (InCell == null) continue;
+                if (InCell.type == CellType.In && InCell != lastHistoryCell) //必然有一个Cell不是上一次访问的点 
+                {
+                    InCellDir = dir;
                     break;
-                case CrossDir.Right:
-                    break;
-                case CrossDir.Up:
-                    break;
-                case CrossDir.Down:
-                    break;
-                default:
-                    Debug.Log("OneInFour err");
-                    return FSM.Error;
+                }
             }
+            if (InCellDir == FourDir._Ct) { Debug.Log("TwoInFour err"); return FSM.Error; }
+
+            //确定下一个移动方向 
+            var nextDir = TwoInFourLUT[aCtx.lastCrossDir]; 
+            if (nextDir == CrossDir.Undifined) { Debug.Log("TwoInFour err"); return FSM.Error; }
+
+            //重置上轮参数 
+            aCtx.UpdateNextWorkingFourParams(nextDir);
+
+            //更新轮廓线追踪状态(调用内部可能会触发输出采样)
+            aCtx.UpdateTracker(OutlineState.Sequence, InCell); //涉及输出采样点，一律使用InCell 
+
+            //注册访问过的Cell
+            aCtx.RegisterHistoryCell(InCell);
+
+            return FSM.CalFour;
+        }
+
+        public FSM ThreeInFour(Context aCtx)
+        {
+            if (aCtx.lastCrossDir == CrossDir.Undifined) { Debug.Log("ThreeInFour err"); return FSM.Error; }
+
+            //确定 OutCell 在哪一个方位 
+            FourDir OutCellDir = FourDir._Ct;
+            Cell OutCell = null;
+            //Cell lastHistoryCell = aCtx.History[aCtx.History.Count - 1];
+            foreach (FourDir dir in dirArray)
+            {
+                if (dir == FourDir._Ct) continue;
+                OutCell = aCtx.WorkingFour[(int)dir];
+                if (OutCell == null || OutCell.type == CellType.Out)
+                {   //null对应边界之外的情况，目前还不兼容贴边描线，不过这里不影响计算也就无所谓了 
+                    OutCellDir = dir;
+                    break;
+                }
+            }
+            if (OutCellDir == FourDir._Ct) { Debug.Log("ThreeInFour err"); return FSM.Error; }
+
 
 
 
@@ -420,8 +571,10 @@ namespace Babeltime.Utils
                         currentState = CalFour(ctx);
                         break;
                     case FSM.OneInFour:
+                        currentState = OneInFour(ctx); 
                         break;
                     case FSM.TwoInFour:
+                        currentState= TwoInFour(ctx);
                         break;
                     case FSM.ThreeInFour:
                         break;
